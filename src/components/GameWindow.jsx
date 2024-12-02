080eb74ef016975355b06882bb6c501d64b4443e
Grumfyyy <swadijosef@gmail.com>
2024-12-02 11:51:04 +0100

    Hardcoded the look and fixed the zooming on mobile devices
    Will later breakout the hardcoded bits to components

--- src/components/GameWindow.jsx
+++ src/components/GameWindow.jsx
@@ -3,7 +3,23 @@ import React from 'react';
 function GameWindow(){
     return (
     <main>
-        <h1>Hello World</h1>
+        <div className='game-info-window'>
+            <h1 id='timer'>30s</h1>
+            <h1 id='score'>5 / 10</h1>
+            <div className='element-card-container'>
+                <div className="element-card">
+                    H
+                </div>
+            </div>
+            <h1 id='question'>What do you call this element?</h1>
+        </div>
+
+        <div id='game-answer-buttons-container'>
+            <button id="game-answer-button-1">Cobalt</button>
+            <button id="game-answer-button-2">Hafnium</button>
+            <button id="game-answer-button-3">Tungsten</button>
+            <button id="game-answer-button-4">Hydrogen</button>
+        </div>
     </main>);
 }
 
